2f017d5b94d038cad00ed840be428b998347437f
GAURAV BHATT <51162173+devb113@users.noreply.github.com>
2021-04-02 17:49:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let pianoKeys = new Set('ASDFGHJWETYU');
 
 document.addEventListener("keypress", function (e) {
     if (pianoKeys.has(e.key.toUpperCase())) {
-        new Audio(e.key.toUpperCase() + ".mp3").play();
+        new Audio("Audio/"+e.key.toUpperCase() + ".mp3").play();
     } else{
         console.warn("Wrong key is pressed")
     }
